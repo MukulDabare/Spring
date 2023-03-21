b153e34be3f95fb5fd76faee5d8413b19b932ea9
MukulDabare <dabare03@gmail.com>
2023-03-21 12:45:48 +0530

    My First Commit

---
+++ Spring_task1/src/com/client/Test.java
@@ -0,0 +1,21 @@
+package com.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.model.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+        //By using Core Container
+		Resource r = new ClassPathResource("beans.xml");
+		BeanFactory bf = new XmlBeanFactory(r);
+		Student s = bf.getBean("s",Student.class);
+		s.display();
+		
+	}
+
+}
---
+++ Spring_task1/src/com/model/Student.java
@@ -0,0 +1,14 @@
+package com.model;
+
+public class Student {
+	
+	public Student() {
+		
+		System.out.println("Student Constructor Task 1");
+	}
+	public void display()
+	{
+		System.out.println("Display method for task 1");
+	}
+
+}
